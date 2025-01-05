58c6f30eafe33cecd6fda3c2374b9a37065e3296
PrajwalKhatokar <prajwal4830@gmail.com>
2025-01-05 23:55:20 +0530

    LBP5

---
+++ daily challenges/src/IncrementValue.java
@@ -0,0 +1,10 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class IncrementValue {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println(++n);
+    }
+}
